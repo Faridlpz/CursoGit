22b531f0fad9512dac6be6983ca71168bdfa394b
Farid Josua Olmedo Lopez <68607809+Faridlpz@users.noreply.github.com>
2022-03-18 12:10:23 -0600

    to read parametrizado from jenkins

---
+++ parametrizadoDSL.groovy
@@ -0,0 +1,20 @@
+job('DSL_2_job') {
+  description('description changed to new one')
+  parameters {
+     booleanParam('FLAG', true)
+     choiceParam('OPTION', ['option 1 (default)', 'option 2', 'option 3'])
+   
+ } 
+  scm {
+    git('https://github.com/Faridlpz/CursoGit.git','main') { node -> 
+    
+    node / gitConfigName('jenkins')  
+    node / gitConfiEmail('neymarchespirito@outook.com') 
+    }
+  }
+  steps{
+  	shell("echo 'Hello wordl'")
+  	
+  }
+  
+}
